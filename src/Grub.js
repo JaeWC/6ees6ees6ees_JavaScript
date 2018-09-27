345e95675bc20e803600f9113cf361f7b35dc1ca
Jae Choi <j.thechois@gmail.com>
2018-09-27 15:52:17 +0900

    Grub Class fix lint style

--- src/Grub.js
+++ src/Grub.js
@@ -1,10 +1,10 @@
 class Grub {
-  constructor(age, color, food) {
+  constructor (age, color, food) {
     this.age = 0
     this.color = 'pink'
     this.food = 'jelly'
   }
-  eat() {
+  eat () {
     return 'Mmmmmmmmm jelly'
   }
 }
